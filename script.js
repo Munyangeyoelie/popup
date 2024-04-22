38dfa9550faec834467d35c0633c8fd76e63878a
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-04-22 17:31:15 +0300

    functionality

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// variable
 const openBtn = document.getElementById("open-btn");
 const closeBtn = document.getElementById("close-btn");
 const modalCantainer = document.getElementById("modal-container");
+
+//functionality
+openBtn.addEventListener("click", function () {
+  modalCantainer.style.display = "block";
+});
+closeBtn.addEventListener("click", function () {
+  modalCantainer.style.display = "none";
+});
+window.addEventListener("click", function (e) {
+  if (e.target == modalCantainer) {
+    modalCantainer.style.display = "none";
+  }
+});
